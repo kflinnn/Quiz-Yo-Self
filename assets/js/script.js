1d516086a2756eb22d432aaf20fe3022e7d952f8
kflinnn <kflinncoding@gmail.com>
2022-11-30 20:41:08 -0500

    created index.html, css, js files. Started work on html and pulled in miniproject code as kick off point

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// //open page to Coding Quiz 
+
+// //click the start button
+
+// //timer starts and presented with question
+
+// //the user selects an answer
+
+// //correct answer brings about another question
+
+// //incorrect question subtracts time from the clock
+
+// //Game ends when all questions are answered or the timer reaches 0
+
+// //User can save their initials and score
+
+// //User is presented with high score list
