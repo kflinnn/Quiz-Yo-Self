adcd03bde12b8dc798c11a62edf14c049358762e
kflinnn <kflinncoding@gmail.com>
2022-12-05 16:59:46 -0500

    added questions, updated CSS, added other items

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var quizDesc = document.querySelector(".quiz-description");
 var startButton = document.querySelector(".start-button");
 var viewHighScores = document.querySelector(".view-highscores")
 var timerEl = document.querySelector(".quiz-timer");
+var submitButton = document.querySelector(".submit-button");
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
 
 var timer;
 var timerCount;
@@ -17,6 +20,7 @@ var timerCount;
 // //init function ?
 function init() {
     timerCount = 0; 
+    submitButton.disabled = true;
 }
 // //click the start button to begin quiz
 startButton.addEventListener("click", startQuiz);
@@ -24,11 +28,104 @@ startButton.addEventListener("click", startQuiz);
 function startQuiz() {
     timerCount = 75;
     startButton.disabled = true;
-    //pullQuestion();
+    pullQuestion();
     startTimer ();
 }
 
+// //array of questions and answers
+var questions = [
+  {question: "What is known as the most basic coding language?",
+  answers: [
+    "a: CSS",
+    "b: JavaScript",
+    "c: HTML",
+  ],
+  correctAnswer: "c"
+  },
+  {question: "What must you link to your HTML in order to see stylistic changes to the content using CSS?",
+  answers: {
+    a: "style.css",
+    b: "reset.css",
+    c: "scipt.js",
+  },
+  correctAnswer: "b"
+  },
+  {question: "What element allows you to store groups of data in a single variable?",
+  answers: {
+    a: "An array",
+    b: "An object",
+    c: "A function",
+  },
+  correctAnswer: "a"
+  },
+  {question: "Which positioning refers to when an element is in normal flow, left to right/top to bottom?",
+  answers: {
+    a: "Static",
+    b: "Fixed",
+    c: "Absolute",
+  },
+  correctAnswer: "a"
+  },
+  {question: "What symbol is used to concatenate?",
+  answers: {
+    a: ":",
+    b: "=",
+    c: "+",
+  },
+  correctAnswer: "c"
+  },
+  {question: "What is a global scope?",
+  answers: {
+    a: "Pertains to one specific function",
+    b: "Affects all functions",
+    c: "Helps make your site user friendly",
+  },
+  correctAnswer: "b"
+  },
+  {question: "Please identify the parent in var changeP = document.getElementById(hero)",
+  answers: {
+    a: "answer changeP",
+    b: "answer document",
+    c: "answer hero",
+  },
+  correctAnswer: "b"
+  },
+  {question: "Which of the below is the name of a Third Party API?",
+  answers: {
+    a: "Bootstrap",
+    b: "CSS",
+    c: "JavaScript",
+  },
+  correctAnswer: "a"
+  },
+ ]
 
+// //Function to pull a question
+ function pullQuestion () {
+  var output = [];
+  var answers;
+
+  for (var i=0; i<questions.length; i++){
+    answers = [];
+
+  for (letter in questions[i].answers){
+    answers.push(
+      '<label>'
+        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+        + letter + ': '
+        + questions[i].answers[letter]
+      + '</label>'
+    );
+  }
+  }
+  output.push(
+    `<div class="question"> ${currentQuestion.question} </div>
+    <div class="answers"> ${answers.join('')} </div>`
+  );
+  quizContainer.innerHTML = output.join("");
+}
+
+ 
 
 // //timer starts and presented with question
 // function startTimer() {
